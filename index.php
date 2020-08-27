edf25329db2a4f5efea6819b7816e048833648ac
rungo728 <rufii72865@gmail.com>
2020-08-27 20:23:03 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="./style.css" >
+  <title>デイトラ PHP</title>
+</head>
+<body>
+  <div class="container">
+    <div class="app-container">
+      <h1 class="title">DailyTrial Shopping</h1>
+      <div class="cards-container">
+        <div class="card">
+          <img class="card-image" src="https://dnbz0c2oupsw6.cloudfront.net/bcekt8ctzrsfdj1gsus49v9tnhqu" alt="">
+          <p class="card-title">ミネラルウォーター</p>
+          <div class="flex justify-between">
+            <p class="card-price">230円</p>
+            <input min="0" class="item-number" type="number" value="0">
+          </div>
+        </div>
+      </div>
+      <div class="btn-footer bg-white">
+        <input class="cart-btn" type="submit" name="submit" value="カートに追加" />
+      </div>
+    </div>
+  </div>
+</body>
+</html>
