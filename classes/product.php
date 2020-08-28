028b76c5dc9ffb9e17767e65546786543de1f3e4
rungo728 <rufii72865@gmail.com>
2020-08-29 01:13:25 +0900

    Create product.php
    class内でのみ取得ができるようにし、それぞれメソッドを定義してそれを活用できるようにする

---
+++ classes/product.php
@@ -0,0 +1,33 @@
+<?php
+  // クラスの作成
+  class Product{
+    // privateにすることによってインスタンスから取得も書き込みもすることができなくなる
+    // ただこのclass内ではプロパティを取得することができる
+    private $id;
+    private $name;
+    private $price;
+    private $image;
+    
+    public function __construct($id,$name,$price,$image){
+      // ここでの$thisはProductのnameの値を取得するという意味
+      // ->nameでプロパティのnameを指定する
+      $this->id = $id;
+      $this->name = $name;
+      $this->price = $price;
+      $this->image = $image;
+    } 
+    public function getId(){
+      return $this->id;
+    }
+    public function getName(){
+      return $this->name;
+    }
+    public function getPrice(){
+      return $this->price;
+    }
+    public function getImage(){
+      return $this->image;
+    }
+  }
+
+?>
