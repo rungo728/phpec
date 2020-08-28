5c3cc8e7b6675cdd7d706580fc55c20a1d111700
rungo728 <rufii72865@gmail.com>
2020-08-28 14:08:39 +0900

    create products.php
    商品情報呼び出し用のphpを作成

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
-<?php
-  $products = [
-    [
-      "name" => "アボカド",
-      "price" => 230,
-      "image" => "./images/image_1.jpg"
-    ],
-    [
-      "name" => "オレンジ",
-      "price" => 300,
-      "image" => "./images/image_2.jpg"
-    ],
-    [
-      "name" => "レモン",
-      "price" => 400,
-      "image" => "./images/image_3.jpg"
-    ],
-    [
-      "name" => "パイナップル",
-      "price" => 500,
-      "image" => "./images/image_4.jpg"
-    ],
-  ];
+<?php require_once 'products.php' ?>
 
-?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
---
+++ products.php
@@ -0,0 +1,25 @@
+<?php
+  $products = [
+    [
+      "name" => "アボカド",
+      "price" => 230,
+      "image" => "./images/image_1.jpg"
+    ],
+    [
+      "name" => "オレンジ",
+      "price" => 300,
+      "image" => "./images/image_2.jpg"
+    ],
+    [
+      "name" => "レモン",
+      "price" => 400,
+      "image" => "./images/image_3.jpg"
+    ],
+    [
+      "name" => "パイナップル",
+      "price" => 500,
+      "image" => "./images/image_4.jpg"
+    ],
+  ];
+
+?>
