40bc86e5ff2e0a44b9475d89a2bbd5b329417e80
rungo728 <rufii72865@gmail.com>
2020-08-29 01:20:21 +0900

    Update index.php
    products.phpにそれぞれのメソッドを定義したので、それに伴って修正

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
-<?php require_once 'products.php' ?>
-<?php require_once 'function.php' ?>
+<?php require_once 'products.php'; ?>
+<?php require_once 'function.php'; ?>
 
-<!DOCTYPE html>
+
+ <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
@@ -12,18 +13,18 @@
 <body>
   <div class="container">
     <div class="app-container">
-      <h1 class="title">DailyTrial Shopping</h1>
+      <h1 class="title">DailyTrial Shopping </h1>
       <!-- 購入商品のデータをcart.phpへ保存する -->
       <form id="cart" method="post" action="cart.php" class="">
         <div class="cards-container">
           <?php foreach($products as $product): ?>
             <div class="card">
-              <img class="card-image" src="<?php echo $product["image"]; ?>" alt="">
-              <p class="card-title"><?php echo $product["name"]; ?></p>
+              <img class="card-image" src="<?php echo $product->getImage(); ?>" alt="">
+              <p class="card-title"><?php echo $product->getName(); ?></p>
               <div class="flex justify-between">
-                <p class="card-price"><?php echo displayPrice($product["price"]);?></p>
+                <p class="card-price"><?php echo displayPrice($product->getPrice());?></p>
                 <!-- idを固有の値にするため、nameに記述 -->
-                <input name="<?php echo $product["id"] ?>" min="0" class="item-number" type="number" value="0">
+                <input name="<?php echo $product->getId(); ?>" min="0" class="item-number" type="number" value="0">
               </div>
             </div>
           <?php endforeach; ?>
